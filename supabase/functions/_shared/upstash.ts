f3a7396cd37270502e4c2cbd46e412227fbdb9c0
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-10-11 18:19:39 +0200

    Add Upstash functions

---
+++ supabase/functions/_shared/upstash.ts
@@ -0,0 +1,57 @@
+import { Redis } from 'npm:@upstash/redis'
+import { Ratelimit } from 'https://cdn.skypack.dev/@upstash/ratelimit@latest'
+
+export function createRedis() {
+  const url = Deno.env.get('UPSTASH_REDIS_REST_URL') || ''
+  const token = Deno.env.get('UPSTASH_REDIS_REST_TOKEN') || ''
+
+  if (!url || !token) {
+    throw new Error('Redis url OR token could not be found.')
+  }
+  return new Redis({
+    url,
+    token,
+  })
+}
+
+export function createRateLimiter(attempts: number, duration: string, identifier: string, opts: UpstashRateLimitOptions = {}): UpstashRateLimitResponse {
+  // Create a new Redis ratelimiter
+  const redis = createRedis()
+
+  const defaultOpts = {
+    analytics: true,
+    /**
+     * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+     * instance with other applications and want to avoid key collisions. The default prefix is
+     * "@upstash/ratelimit"
+     * Override it by passing an object as the 4th param
+     */
+    prefix: '@upstash/ratelimit',
+  }
+
+  const options: UpstashRateLimitOptions = { ...defaultOpts, ...opts }
+
+  const upstashRateLimit: UpstashRateLimitResponse = new Ratelimit({
+    redis,
+    limiter: Ratelimit.slidingWindow(
+      attempts, // how many times you can do something
+      duration // in given time frame
+    ),
+    ...options,
+  }).limit(identifier)
+
+  return upstashRateLimit
+}
+
+interface UpstashRateLimitResponse {
+  success: boolean
+  limit: number
+  remaining: number
+  reset: number
+  pending: object
+}
+
+interface UpstashRateLimitOptions {
+  analytics?: boolean
+  prefix?: string
+}
